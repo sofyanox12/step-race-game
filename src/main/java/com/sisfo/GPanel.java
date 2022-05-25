610916bca440dce727f8a869d54f0a791f64635a
sofyanox12 <gaero38@gmail.com>
2022-05-25 23:57:00 +0800

    commit on 'Master'

--- src/main/java/com/sisfo/GPanel.java
+++ src/main/java/com/sisfo/GPanel.java
@@ -1,59 +1,92 @@
 package com.sisfo;
 
+import javafx.application.Platform;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
 /*
-    Peran Kelas GPanel disini adalah sebagai konfigurasi
+    Peran Kelas GPanel disini adalah sebagai Engine Game
     utama dan juga sebagai ekstender dari library Canvas
     sebagai jendela untuk gamenya.
 */
 
-public class GPanel extends Canvas implements Runnable {
+public class GPanel extends Canvas {
 
-    /* ## Konfigurasikan ## */
-    
+    final Canvas canvas;
+
+    /* ##--- Konfigurasi Ukuran Engine ---## */
+
     final int tileSize = 16; // 16x16 Ukuran Tile - sesuaikan
-    final double charScale = 3;
+    final int charScale = 3;
 
-    final double spriteSize = tileSize * charScale; // 32x2 = 64 Ukuran pixel
+    final int spriteSize = tileSize * charScale; // 16x3 = 48 Ukuran pixel
 
-    final int maxPanelCol = 16;
-    final int maxPanelRow = 12;
+    final int maxPanelCol = 16; // 16 tile melebar kekanan (horizontal)
+    final int maxPanelRow = 12; // 12 tile melebar kebawah (vertikal)
 
-    final double panelWidth = spriteSize * maxPanelCol; // 64x16 = 1024 Ukuran pixel
-    final double panelHeigth = spriteSize * maxPanelRow; // 64x12 = 768 Ukuran pixel
+    final int panelWidth = spriteSize * maxPanelCol; // 48x16 = 768 Ukuran pixel
+    final int panelHeigth = spriteSize * maxPanelRow; // 48x12 = 576 Ukuran pixel
 
-    private Thread gameRunning;
-
+    /* ##-------------------------------## */
+    
+    private GraphicsContext graphic = getGraphicsContext2D();
+    
+    private final long FIRST_SECOND = System.currentTimeMillis();
+   
+    public Thread gameRunning;
+ 
     public GPanel() {
 
+        this.canvas = new Canvas();
         this.setHeight(panelHeigth);
         this.setWidth(panelWidth);
+
+        Thread thread = new Thread(() -> {
+
+            Runnable updater = () -> { // Isi Method untuk mengupdate window disini :
+
+                update();
+                render(graphic);
+                
+            };
+            
+            while (true) { // Isi Method Background Disini :
+                
+                System.out.printf("\nGame Berjalan... [%s s]", getSecond());
+                
+                Platform.runLater(updater);
+                
+            }
+        });
         
+        thread.setDaemon(true);
+        thread.start();
     }
 
-    public void startGameThread() {
-        gameRunning = new Thread(this); // Menyatakan class "GPanel" sebagai Thread
-        gameRunning.start();
-        
+    public int getSecond() {
+        long now = System.currentTimeMillis();
+        return (int)((now - FIRST_SECOND) / 1000);
     }
 
-    @Override
-    public void run() { // Isi game :
-        
-        while(gameRunning != null) {
 
-            System.out.println("Game sedang berjalan");
-
-        }
-    }
+    // Engine Game disini :
 
     public void update() {
 
     }
 
     public void render(GraphicsContext g) {
-        
+
+        g.setFill(Color.WHITE);
+        g.fillRect(100, 100, spriteSize, spriteSize);
+
     }
+
+
+    
+    
+    
+
+    
 }
--- src/main/java/com/sisfo/StepRace.java
+++ src/main/java/com/sisfo/StepRace.java
@@ -2,38 +2,43 @@ package com.sisfo;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-/**
- * JavaFX App
- */
 public class StepRace extends Application {
-
-    private Pane root;
-
     
-
     public static void main(String[] game) {
         launch(game);
     }
 
-    public void start(Stage stage) {
 
-        stage.setTitle("Step Race");
+    public void start(Stage stage) {
+        
+        Image icon = new Image("file:src/main/resources/com/sisfo/icon.png");
 
         GPanel window = new GPanel();
+        
+        Pane root = new Pane(window);
 
-        //GraphicsContext graphicsContext = window.getGraphicsContext2D();
+        root.setStyle("-fx-background-color: #000000;");
+        stage.setTitle("Step Race");
 
-        root = new Pane(window);
+        Button testButton = new Button("Test");
+        root.getChildren().add(testButton);
+        //root.setStyle("-fx-background-color: #000000;");
+
+        //window.startGameThread(); // Jalankan Thread
+
 
         Scene scene = new Scene(root, window.panelWidth, window.panelHeigth);
 
+        stage.getIcons().add(icon);
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
-
 
 
 }
