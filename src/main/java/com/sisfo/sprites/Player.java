a05a6fbba344859908eb8c81a87330dfabc6cd82
sofyanox12 <gaero38@gmail.com>
2022-05-29 00:37:45 +0800

    Menambah mekanisme pergantian Pemain

--- src/main/java/com/sisfo/sprites/Player.java
+++ src/main/java/com/sisfo/sprites/Player.java
@@ -8,7 +8,8 @@ import javafx.scene.image.Image;
 
 public class Player extends Entity {
 
-    public final int mapView1_X;
+    public static int moves = 0;
+    public static int mapShift = 0;
 
     private final Image[] walkP1 = new Image[6];
     private final Image[] idleP1 = new Image[4];
@@ -19,30 +20,23 @@ public class Player extends Entity {
     private final GameEvent event;
     private final GPanel window;
 
-    private Boolean idlingP1 = false;
-    private Boolean idlingP2 = true;
+    public static Boolean idlingP1 = true;
+    public static Boolean idlingP2 = true;
 
-    public Boolean computerPlay = false;
+    public Boolean computerPlay = true;
 
     public int move, maxMove;
 
-    public Player(GPanel window, GameEvent event) {
+    public Player(GPanel window, GameEvent event) { // Kunci dari semua ini
 
         this.window = window;
         this.event = event;
-
-        mapView1_X = (window.panelWidth - window.spriteSize) / 2;
-
-        event.diceRoll();
-
-        setPlayerStartingPos(window);
-        generateTraps();
-        getPlayer(3, 1); // p1
-        getPlayer(4, 2); // p2
-
-        this.move = event.moves;
-
-        maxMove = PLAYER1_X + (this.move * window.spriteSize);
+        this.setPlayerStartingPos(window);
+        this.generateTraps();
+        this.getPlayer(1, 1);
+        this.getPlayer(4, 2);
+        this.diceRoll();
+        this.stopPlayer();
 
     }
 
@@ -64,13 +58,13 @@ public class Player extends Entity {
                 spriteCounterP1++;
 
             } else {
-                spriteNumP1 --;
+                spriteNumP1--;
                 stopPlayer();
             }
         }
 
-        else  {
-            if (spriteCounterP1 > 5) { // Delay per animasi Karakter
+        else {
+            if (spriteCounterP1 > 10) { // Delay per animasi Karakter
                 spriteNumP1 += (spriteNumP1 < idleP1.length) ? 1 : 0;
                 spriteNumP1 = (spriteNumP1 == idleP1.length) ? 1 : spriteNumP1;
                 spriteCounterP1 = 0;
@@ -78,7 +72,9 @@ public class Player extends Entity {
             spriteCounterP1++;
         }
 
-        System.out.println("\tPlayer1 ::> " + spriteNumP1 + " " + idlingP1);
+        if (mapShift < 0)
+            mapShift = 0;
+        // System.out.println("\tPlayer1 ::> " + spriteNumP1 + " " + idlingP1);
 
     }
 
@@ -86,9 +82,9 @@ public class Player extends Entity {
 
         if (!idlingP2) {
 
-            if (PLAYER1_X < maxMove) {
+            if (PLAYER2_X < maxMove) {
 
-                PLAYER1_X++;
+                PLAYER2_X++;
 
                 if (spriteCounterP2 > 5 && !idlingP2) { // Delay per animasi Karakter
                     spriteNumP2 += (spriteNumP2 < walkP2.length) ? 1 : 0;
@@ -98,13 +94,13 @@ public class Player extends Entity {
                 spriteCounterP2++;
 
             } else {
-                spriteNumP2 --;
+                spriteNumP2--;
                 stopPlayer();
             }
         }
 
-        else  {
-            if (spriteCounterP2 > 5) { // Delay per animasi Karakter
+        else {
+            if (spriteCounterP2 > 10) { // Delay per animasi Karakter
                 spriteNumP2 += (spriteNumP2 < idleP2.length) ? 1 : 0;
                 spriteNumP2 = (spriteNumP2 == idleP2.length) ? 1 : spriteNumP2;
                 spriteCounterP2 = 0;
@@ -112,8 +108,7 @@ public class Player extends Entity {
             spriteCounterP2++;
         }
 
-
-        System.out.print("\nPlayer2 ::> " + spriteNumP2 + " " + idlingP2);
+        // System.out.print("\nPlayer2 ::> " + spriteNumP2 + " " + idlingP2);
     }
 
     public void drawP1(GraphicsContext render) {
@@ -121,12 +116,16 @@ public class Player extends Entity {
         Image player1 = null;
 
         if (idlingP1 && spriteNumP1 < idleP1.length) {
+            spriteNumP1 = (spriteNumP1 < 1) ? 1 : spriteNumP1;
             player1 = idleP1[spriteNumP1 - 1];
 
         } else if (!idlingP1 && spriteNumP1 < walkP1.length) {
+            spriteNumP1 = (spriteNumP1 < 1) ? 1 : spriteNumP1;
             player1 = walkP1[spriteNumP1 - 1];
 
         }
+        // render.drawImage(player1, PLAYER1_X, PLAYER1_Y, window.spriteSize,
+        // window.spriteSize);
         render.drawImage(player1, PLAYER1_X, PLAYER1_Y, window.spriteSize, window.spriteSize);
     }
 
@@ -135,9 +134,11 @@ public class Player extends Entity {
         Image player2 = null;
 
         if (idlingP2 && spriteNumP2 < idleP2.length) {
+            spriteNumP2 = (spriteNumP2 < 1) ? 1 : spriteNumP2;
             player2 = idleP2[spriteNumP2 - 1];
 
         } else if (!idlingP2 && spriteNumP2 < walkP2.length) {
+            spriteNumP2 = (spriteNumP2 < 1) ? 1 : spriteNumP2;
             player2 = walkP2[spriteNumP2 - 1];
 
         }
@@ -146,9 +147,15 @@ public class Player extends Entity {
     }
 
     public void stopPlayer() {
+        
         computerPlay = (computerPlay) ? false : true;
-        PLAYER1_SCORE += (playerID == 1) ? event.moves : 0;
-        PLAYER2_SCORE += (playerID == 2) ? event.moves : 0;
+        playerID = (playerID == 1) ? 2 : 1;
+        if (moves > 1) {
+            PLAYER1_SCORE += (playerID == 1) ? event.moves : 0;
+            PLAYER2_SCORE += (playerID == 2) ? event.moves : 0;
+        } else {
+            this.maxMove = 0;
+        }
         idlingP1 = true;
         idlingP2 = true;
         playerID = 0;
@@ -158,6 +165,22 @@ public class Player extends Entity {
         System.out.println("Score Player 1 : " + PLAYER1_SCORE);
         System.out.println("Score Player 2 : " + PLAYER2_SCORE);
 
+        if (computerPlay) {
+            playerID = 2;
+            idlingP2 = false;
+            diceRoll();
+        }
+    }
+
+    public void diceRoll() {
+        this.event.diceRoll();
+        this.move = event.moves;
+        if (playerID == 1) {
+            this.maxMove = PLAYER1_X + (this.event.moves * window.spriteSize);
+        } else if (playerID == 2) {
+            this.maxMove = PLAYER2_X + (this.event.moves * window.spriteSize);
+        }
+        
     }
 
     public void getPlayer(int x, int playerID) {
